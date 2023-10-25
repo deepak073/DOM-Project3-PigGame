c0a59bd8843d4a83f9475bae8b396336e75861b9
deepak073 <89341816+deepak073@users.noreply.github.com>
2023-10-25 14:46:40 +0000

    Implemented HOLD button to add score,switch player

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 //Selecting Element
 
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
-
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
@@ -22,8 +21,19 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let totalScore = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 // Rolling dice functionality
 
@@ -46,11 +56,23 @@ btnRoll.addEventListener("click", function () {
       currentScore;
   } else {
     // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  // add the current score to main score
+
+  scores[activePlayer] += currentScore;
+  // if (totalScoreScore<currentScore)
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. check if player score is 100
+  // if (scores[activePlayer] >=100){
+
+  // }
+
+  // 3. switch player on Hold butten press
+  switchPlayer();
 });
